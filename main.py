428767e93fff168e23d4ba8ae0d143fd2df1f018
Manita Bista <xettriib222@gmail.com>
2026-02-27 23:53:55 -0600

    added a function

--- main.py
+++ main.py
@@ -1,22 +1,44 @@
-#def budget():
-    #categories: food, rent, utilities, transportation, shopping, entertainment, savings
-import json
+# #def budget():
+#     #categories: food, rent, utilities, transportation, shopping, entertainment, savings
+# import json
 
-# New data to add
-new_data = {"name": "khebde", "age": 21}
+# # New data to add
+# new_data = {"name": "khebde", "age": 21}
 
 
-# Step 1: Read existing data
-with open("data.json", "r") as file:
-    data = json.load(file)
+# # Step 1: Read existing data
+# with open("data.json", "r") as file:
+#     data = json.load(file)
 
-# Step 2: Add new data
-data.append(new_data)
+# # Step 2: Add new data
+# data.append(new_data)
 
-# Step 3: Write back to file
-with open("data.json", "w") as file:
-    json.dump(data, file,indent=4)
+# # Step 3: Write back to file
+# with open("data.json", "w") as file:
+#     json.dump(data, file,indent=4)
 
-print("data append success")
+# print("data append success")
 
-m
+def budget():
+    Categories= {
+        "Month" : "January",
+        "Rent" : 250,
+        "Food" : 100,
+        "Utilities" : 40,
+        "Entertainment" : 10,
+        "Shopping" : 5,
+        "Savings" : 0,
+    }
+    Categories["Month"]=input("Month :")
+    rent=input("Rent :")
+    Food=input("Food :")
+    utilities=input("Utilities :")
+    Shopping=input("Shopping :")
+
+
+    print(Categories["Month"])
+    print(Categories)
+
+  
+
+budget()
